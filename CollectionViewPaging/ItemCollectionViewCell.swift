3aeb0104fdd81e36d380407639155522d60a4888
metawin99 <pai.pp61@gmail.com>
2018-09-21 12:12:26 +0700

    first commit

---
+++ CollectionViewPaging/ItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCollectionViewCell.swift
+//  CollectionViewPaging
+//
+//  Created by Soemsak on 21/9/2561 BE.
+//  Copyright © 2561 Soemsak. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+}
